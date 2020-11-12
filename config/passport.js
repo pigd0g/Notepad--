2546076d254563fa602813271a26022c312bb52e
pigd0g <16750317+pigd0g@users.noreply.github.com>
2020-11-13 10:27:31 +1100

    Switched Password to YubiKey

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,8 @@
 const CustomStrategy = require('passport-custom').Strategy;
+var yub = require('yub');
+
+// initialise the yub library
+yub.init(process.env.YUBI_CLI, process.env.YUBI_KEY);
 
 const my_user = {
   username: 'admin',
@@ -10,12 +14,17 @@ module.exports = function(passport) {
   passport.use('simpleauth', new CustomStrategy(
     function(req, done) {
       
-      if (req.body.password !== my_user.password) {
-        return done(null, false, { message: 'Incorrect password' });           
-      }
-      let err = ''
-      done(err, my_user);
+      yub.verify(req.body.password, function(err,data) {
+        //console.log(err, data)
+        if (err) {
+          return done(null, false, { message: 'Incorrect password' });
+        }
+        if (data.serial != process.env.YUBI_SERIAL || !data.valid) {
+          return done(null, false, { message: 'Incorrect password' });
+        }
 
+        done(err, my_user);
+      });
     }
   ));
   
